2fa5a2f17a3d6e8ce61078e6f7a16d1436e994b3
Luu Gia Thuy <luugiathuy@gmail.com>
2016-01-24 18:24:26 +0800

    Add kindleOnly option to scraper
    When true, only return Kindle books in the list

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 import test from 'ava';
 import AmazonListScraper from '../dist';
 
+const testListURL = 'https://www.amazon.com/gp/registry/wishlist/1JMCNHNT959X2';
+const multiPagesListURL = 'https://www.amazon.com/gp/registry/wishlist/1EL6CUGB5P0ZV';
+
 test('expose a constructor', t => {
   t.is(typeof AmazonListScraper, 'function');
 });
@@ -20,18 +23,18 @@ test('error if url is not found', t => {
 });
 
 test('return list of items with title, price and link', t => {
-  return new AmazonListScraper().scrape('https://www.amazon.com/gp/registry/wishlist/1JMCNHNT959X2').then(items => {
+  return new AmazonListScraper().scrape(testListURL).then(items => {
     t.is(items.length, 2);
     const { title, price, link } = items[0];
-    t.is(title, 'The Principles of Object-Oriente​d JavaScript');
+    t.is(title, `JavaScript: The Good Parts: The Good Parts`);
     t.true(!isNaN(parseFloat(price)));
-    t.true(!!link);
-    t.is(items[1].title, 'Clean Code: A Handbook of Agile Software Craftsmanship');
+    t.true(link.startsWith('https://www.amazon.com/dp/B0026OR2ZY'));
+    t.is(items[1].title, `Clean Code: A Handbook of Agile Software Craftsmanship`);
   });
 });
 
 test('scrape next page of the list', t => {
-  return new AmazonListScraper().scrape('https://www.amazon.com/gp/registry/wishlist/1EL6CUGB5P0ZV').then(items => {
+  return new AmazonListScraper().scrape(multiPagesListURL).then(items => {
     t.is(items.length, 28);
   });
 });
@@ -39,5 +42,14 @@ test('scrape next page of the list', t => {
 test('return empty list if not an Amazon list', t => {
   return new AmazonListScraper().scrape('https://google.com').then(items => {
     t.is(items.length, 0);
+  });
+});
+
+test('return only Kindle books if kindleOnly option is true', t => {
+  const scraper = new AmazonListScraper({ kindleOnly: true });
+  return scraper.scrape(testListURL).then(items => {
+    t.is(items.length, 1);
+    const { title } = items[0];
+    t.is(title, `JavaScript: The Good Parts: The Good Parts`);
   });
 });
